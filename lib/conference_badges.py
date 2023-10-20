061749e96740648254d162e2585027a8957a1a9a
aman githinji <amangithinji09@gmail.com>
2023-10-20 14:43:08 +0300

    Completed assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,25 @@
 def badge_maker(name):
-    return None
+    return (f"Hello, my name is {name}.")
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badge_message = "Hello, my name is " + name + "."
+        badges.append(badge_message) 
+    return badges
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    for i in range(len(names)):
+        room_num = i + 1
+        room_assignment = "Hello, " + names[i] + "! You'll be assigned to room " + str(room_num) + "!"
+        room_assignments.append(room_assignment)
+    return room_assignments
 
 def printer(names):
-    return None
+    messages = batch_badge_creator(names)
+    for message in messages:
+        print(message)
+    assignments = assign_rooms(names)
+    for assignment in assignments:
+        print(assignment)
